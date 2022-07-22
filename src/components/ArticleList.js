0c530242e7f8401a9830824ce3e42cd5d0a70ffb
mitchelle <mitchellenjoki7@gmail.com>
2022-07-22 13:00:52 +0300

    Done.

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import Article from "./Article"
+
+function ArticleList(props) {
+    const postsArray = props.posts.map(post => {
+        return <Article 
+            key={post.id}
+            title={post.title}
+            date={post.date}
+            preview={post.preview}
+            />
+    })
+
+    return (
+        <main>
+            {postsArray}
+        </main>
+    )
+}
+
+export default ArticleList; 
